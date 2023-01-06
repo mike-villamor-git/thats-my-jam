c60d913d31c825cc155a45e720675c8764d1018a
Michael Villamor <michael.villamor300@gmail.com>
2023-01-06 18:04:47 -0500

    added functionality to add points for selecting the right jams

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ import Menu from './Menu'
 function App(){
     const [gameStarted, setGameStarted] = useState(false)
     const [menuOpen, setMenuOpen] = useState(false)
+    const [points, setPoints] = useState(0);
    
-    
+    const addPoints = () => {
+        setPoints(points => points + 100)
+    }
 
     const startGame = () => {
         setGameStarted(gameStarted => !gameStarted)
@@ -18,8 +21,8 @@ function App(){
     return(
     <div className="App">
         <div className={gameStarted ? "map gameStart" : "map"}>
-            {gameStarted ? <NavBar menuOpened={openMenu}/> : null}
-            {gameStarted ? <Game /> : <button onClick={startGame}>Click To Play!</button>}
+            {gameStarted ? <NavBar points={points} menuOpened={openMenu}/> : null}
+            {gameStarted ? <Game points={points} addPoints={addPoints}/> : <button onClick={startGame}>Click To Play!</button>}
         </div>
         { menuOpen ? <div className="Menu">
             <Menu quitGame={startGame} closeMenu={openMenu}/>
