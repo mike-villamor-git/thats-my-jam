6574f58219f022c5166678b321db8420d8fa6f8f
Jasmine Linang <jasminelinang@jasmines-air-2.mynetworksettings.com>
2023-01-06 20:16:20 -0500

    The buttons look nice now. Also how to play menu

---
+++ src/HowToPlay.jsx
@@ -0,0 +1,14 @@
+import './HowToPlay.css'
+
+function HowToPlay(props){
+    return(
+        <div className="HTP">
+             <h2>How To Play</h2>
+             <p1> Each customer will order jams and you're tasked with providing the correct one. Serve as much customers until the timer runs out.</p1>
+             <img class="imageBot" src="/src/assets/jamGame/howToPlay.PNG" width="350px"/>
+        </div>
+        
+    )
+}
+
+export default HowToPlay
